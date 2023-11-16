5c21ffa1b65df7c61b8e0d5112294127b7822ed4
petergreutmann <petergreutmann@bluewin.ch>
2023-11-16 08:37:16 +0100

    "NOW" gross geschrieben

--- main.ts
+++ main.ts
@@ -7,5 +7,5 @@ console.log(mySecondCode)
 const myThirdCode : string = "No more automatens"
 console.log(myThirdCode)
 
-const myFourthCode : string = "Maybe it works now."
+const myFourthCode : string = "Maybe it works NOW."
 console.log(myFourthCode)
