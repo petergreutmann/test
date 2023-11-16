b164afa7906a341c5f2aeba7dbe8eddc9b5ea722
petergreutmann <petergreutmann@bluewin.ch>
2023-11-16 08:43:53 +0100

    Kompiliert

---
+++ main.js
@@ -0,0 +1,8 @@
+var myFirstCode = "Hello World";
+console.log(myFirstCode);
+var mySecondCode = "Still here";
+console.log(mySecondCode);
+var myThirdCode = "No more automatens";
+console.log(myThirdCode);
+var myFourthCode = "Maybe it works NOW.";
+console.log(myFourthCode);
